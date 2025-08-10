cf5d83aa729d6b106b46ce00f035ebf756db21cd
Shahid Khan <shd6161@gmail.com>
2025-08-10 21:16:00 +0000

    padding issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Footer } from "@/components/footer";
 import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
-  const sectionClass = "container py-16";
+  const sectionClass = "container mx-auto px-4 py-16";
 
   return (
     <div className="flex min-h-screen flex-col">
@@ -19,7 +19,7 @@ export default function Home() {
         </section>
 
         {/* Separator */}
-        <div className="container">
+        <div className="container mx-auto px-4">
           <Separator />
         </div>
 
@@ -29,7 +29,7 @@ export default function Home() {
         </section>
 
         {/* Separator */}
-        <div className="container">
+        <div className="container mx-auto px-4">
           <Separator />
         </div>
 
@@ -39,12 +39,12 @@ export default function Home() {
         </section>
 
         {/* Separator */}
-        <div className="container">
+        <div className="container mx-auto px-4">
           <Separator />
         </div>
 
         {/* Contact Section */}
-        <section id="contact" className={`${sectionClass}`}>
+        <section id="contact" className={sectionClass}>
           <div className="mx-auto flex max-w-[58rem] flex-col items-center text-center gap-4">
             <h2 className="text-3xl font-bold leading-[1.1] sm:text-5xl">
               Contact Me
